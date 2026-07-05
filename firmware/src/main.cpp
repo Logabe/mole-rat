a4dc0ddef1a4d2a5694b65e05985ebeb3e86b514
Logan Bentley <logangbentley@gmail.com>
2026-07-05 17:38:04 +0800

    semi-working integration

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,51 +1,90 @@
-#include <Arduino.h>
-#include <BleMouse.h>
-#include <MPU6050_6Axis_MotionApps612.h>
-#include <Wire.h>
+#include "I2Cdev.h"
+// #include "MPU6050_6Axis_MotionApps20.h"
+#include "MPU6050_6Axis_MotionApps612.h" // Uncomment this library to work with DMP 6.12 and comment on the above library.
+#include "BleMouse.h"
 
-#define CALIBRATE
-#define INTERRUPT_PIN D3
-
+/* MPU6050 default I2C address is 0x68*/
 MPU6050 mpu;
-BleMouse mouse("Mole rat");
+BleMouse mouse("MOLE RAT");
 
-/* MPU6050 Stuff */
-bool DMPReady = false; // Flag for loop
-uint8_t MPUIntStatus;
-uint8_t devStatus;
-uint8_t packetSize;
-uint8_t FIFOBuffer[64]; 
+// #define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_EULER
+//#define OUTPUT_READABLE_REALACCEL
+#define OUTPUT_READABLE_WORLDACCEL
+//#define OUTPUT_TEAPOT
 
-Quaternion q; // Quaternion rotation
-VectorInt16 aa; // Acceleration
-VectorInt16 gy; // Gyro
-VectorInt16 aaReal; // Gravity free
-VectorInt16 aaWorld; // World frame
-VectorFloat gravity; // Gravity vector
-float euler[3]; // Euler angles
+int const INTERRUPT_PIN = 2;  // Define the interruption #0 pin
+bool blinkState;
 
-/* Interrupt */
-volatile bool MPUInterrupt = false;
+/*---MPU6050 Control/Status Variables---*/
+bool DMPReady = false;  // Set true if DMP init was successful
+uint8_t MPUIntStatus;   // Holds actual interrupt status byte from MPU
+uint8_t devStatus;      // Return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // Expected DMP packet size (default is 42 bytes)
+uint8_t FIFOBuffer[64]; // FIFO storage buffer
+
+/*---Orientation/Motion Variables---*/ 
+Quaternion q;           // [w, x, y, z]         Quaternion container
+VectorInt16 aa;         // [x, y, z]            Accel sensor measurements
+VectorInt16 gy;         // [x, y, z]            Gyro sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            Gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            World-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            Gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   Yaw/Pitch/Roll container and gravity vector
+
+float vX = 0.0;
+float vY = 0.0;
+
+/*-Packet structure for InvenSense teapot demo-*/ 
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+
+/*------Interrupt detection routine------*/
+volatile bool MPUInterrupt = false;     // Indicates whether MPU6050 interrupt pin has gone high
 void DMPDataReady() {
   MPUInterrupt = true;
 }
 
 void setup() {
-  Wire.begin();
-  Wire.setClock(400000);
-  Serial.begin(115200);
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment on this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
 
   mouse.begin();
-  pinMode(D1, OUTPUT);
-
-  Serial.println("INITING I2C");
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);  
   
-  Serial.println("INITING DMP");
-  devStatus = mpu.dmpInitialize();
-  Serial.println(devStatus);
+  Serial.begin(115200); //115200 is required for Teapot Demo output
+  while (!Serial);
 
+  /*Initialize device*/
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  /*Verify connection*/
+  // Serial.println(F("Testing MPU6050 connection..."));
+  // if(mpu.testConnection() == false){
+  //   Serial.println("MPU6050 connection failed");
+  //   while(true);
+  // }
+  // else {
+  //   Serial.println("MPU6050 connection successful");
+  // }
+
+  /*Wait for Serial input*/
+  Serial.println(F("\nSend any character to begin: "));
+  while (Serial.available() && Serial.read()); // Empty buffer
+  while (!Serial.available());                 // Wait for data
+  while (Serial.available() && Serial.read()); // Empty buffer again
+
+  /* Initializate and configure the DMP*/
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  /* Supply your gyro offsets here, scaled for min sensitivity */
   mpu.setXGyroOffset(0);
   mpu.setYGyroOffset(0);
   mpu.setZGyroOffset(0);
@@ -53,74 +92,81 @@ void setup() {
   mpu.setYAccelOffset(0);
   mpu.setZAccelOffset(0);
 
+  /* Making sure it worked (returns 0 if so) */ 
   if (devStatus == 0) {
-    mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_4);
-    mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
-
-    #ifdef CALIBRATE
-    mpu.CalibrateAccel(10);
-    mpu.CalibrateGyro(10);
+    mpu.CalibrateAccel(6);  // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateGyro(6);
+    Serial.println("These are the Active offsets: ");
     mpu.PrintActiveOffsets();
-    // Serial.print("Gyro Offset: "); Serial.print(mpu.getXGyroOffset()); Serial.print(", "); Serial.print(mpu.getYGyroOffset()); Serial.print(", "); Serial.println(mpu.getZGyroOffset());
-    // Serial.print("Acc Offset"); Serial.print(mpu.getXAccelOffset()); Serial.print(", "); Serial.print(mpu.getYAccelOffset()); Serial.print(", "); Serial.println(mpu.getZAccelOffset());
-    #endif
-    
-
-    Serial.println("ENABLING DMP");
+    Serial.println(F("Enabling DMP..."));   //Turning ON DMP
     mpu.setDMPEnabled(true);
 
-    Serial.println("ENABLING INT");
+    /*Enable Arduino interrupt detection*/
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
     attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), DMPDataReady, RISING);
     MPUIntStatus = mpu.getIntStatus();
 
-    Serial.println("DMP Ready");
-    DMPReady = true;
-    packetSize = mpu.dmpGetFIFOPacketSize();
-    Serial.print("Packet size: "); Serial.println(packetSize);
-  } else {
-    Serial.println("DMP INITIALIZATION FAIL");
-  }
-}
+    /* Filter */
+    // mpu.dmpSetLinearAccelFilterCoefficient(10);
 
-double vX, vY = 0;
+    /* Set the DMP Ready flag so the main loop() function knows it is okay to use it */
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    DMPReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize(); //Get expected DMP packet size for later comparison
+  } 
+  else {
+    Serial.print(F("DMP Initialization failed (code ")); //Print the error code
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+  }
+  // pinMode(LED_BUILTIN, OUTPUT);
+}
 
 ulong lastTime = 0;
 
-// sensors_vec_t rotateVector(sensors_vec_t base, sensors_vec_t rot) {
-//   // Do stuff here
-// }
-
 void loop() {
-  /* Get new sensor events with the readings */
-  if (!DMPReady) return;
+  if (!DMPReady) return; // Stop the program if DMP programming fails.
 
   ulong newTime = millis();
   float deltaTime = (float)(newTime - lastTime) / 1000;
 
-  if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
+  /* Read a packet from FIFO */
+  if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) { // Get the Latest packet 
+
+    /* Display initial world-frame acceleration, adjusted to remove gravity
+    and rotated based on known orientation from Quaternion */
     mpu.dmpGetQuaternion(&q, FIFOBuffer);
-    mpu.dmpGetEuler(euler, &q);
     mpu.dmpGetAccel(&aa, FIFOBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
     mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-  }
+    Serial.print("aworld\t");
+    Serial.print(aaWorld.x);
+    Serial.print("\t");
+    Serial.print(aaWorld.y);
+    Serial.print("\t");
+    Serial.print(aaWorld.z);
 
-  vX = ((float)aaWorld.x / (float)INT16_MAX) * deltaTime * 5000.0;
-  vY = ((float)aaWorld.y / (float)INT16_MAX) * deltaTime * 5000.0;
-  if (vX < 1) vX = 0;
-  if (vY < 1) vY = 0;
-  
-  Serial.print("vX: ");
-  Serial.print(vX);
-  Serial.print(", vY: ");
-  Serial.println(vY);
+    vX += (aaWorld.x / 1000.0) * deltaTime;
+    vY += (aaWorld.y / 1000.0) * deltaTime;
 
-  // double m = (double)millis() / 500.0;
-  // digitalWrite(D1, (byte)(sin(m)* 255.0));
-  if(mouse.isConnected()) {
-    // mouse.move(cos(m) * 3, sin(m) * -3);
-    mouse.move((byte)vY, (byte)vX);
-  }
+    Serial.print("\tvXY\t");
+    Serial.print(vX);
+    Serial.print("\t");
+    Serial.println(vY);
+
+    if (aaWorld.x < 20 && aaWorld.x > -20) vX = 0;
+    if (aaWorld.y < 20 && aaWorld.y > -20) vY = 0;
+
+    mouse.move(vX * deltaTime * 500, vY * deltaTime * 500);
+    
+  /* Blink LED to indicate activity */
+  blinkState = !blinkState;
+  // digitalWrite(LED_BUILTIN, blinkState);
   lastTime = newTime;
+  }
 }
